d7ec2ae3691c5d59b6b23f7c632e84f4e56b5656
Amiya Behera <amiyatulu@gmail.com>
2020-09-12 23:02:11 +0530

    sort

--- percentile.py
+++ percentile.py
@@ -16,6 +16,8 @@ for x in range(1000):
 for x in range(5000):
     num = random.randint(100,200)
     data.append(num)
+
+data.sort()
 # data = [1,3,8,12,15,16,25,33,34,39,44,51,56,60,64,70,82,84,86,90,93,96,97,100]
 percentile = np.arange(0, 100, 5)
 percentile = percentile[1:]
